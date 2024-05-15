d73375426e6e46bd76d78e9764423abfe8c53675
Dana Sharon <whosedreamisthis@gmail.com>
2024-05-14 17:50:58 -0700

    width, height hardcoded values replaced by variables, add walls (border of canvas

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 const { Engine, Render, Runner, World, Bodies } = Matter;
 
+const width = 600;
+const height = 600;
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
-    width: 800,
-    height: 600,
+    wireframes: false,
+    width,
+    height,
+    background: "rgb(255,255,255)",
   },
 });
 
 Render.run(render);
 Runner.run(Runner.create(), engine);
+
+// walls
+const walls = [
+  Bodies.rectangle(width / 2, 0, width, 40, {
+    isStatic: true,
+  }),
+  Bodies.rectangle(width / 2, height, width, 40, {
+    isStatic: true,
+  }),
+  Bodies.rectangle(0, height / 2, 40, height, {
+    isStatic: true,
+  }),
+  Bodies.rectangle(width, height / 2, 40, height, {
+    isStatic: true,
+  }),
+];
+
+World.add(world, walls);
+
+// Maze Generation
